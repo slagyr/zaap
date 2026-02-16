edaefe3388612efd572837b34a2ce6dab3d5cbf6
Micah Martin <micahmartin@zanebot.lan>
2026-02-16 13:54:11 -0700

    Fix: wire MainTabView as root view in ZaapApp

--- Zaap/Zaap/ZaapApp.swift
+++ Zaap/Zaap/ZaapApp.swift
@@ -13,7 +13,7 @@ struct ZaapApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
                 .environment(locationManager)
         }
     }
